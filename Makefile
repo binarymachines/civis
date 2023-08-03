61e384c29cc3da4628f137121ce1f6f9049474fd
Dexter Taylor <binarymachineshop@gmail.com>
2023-08-03 19:12:56 -0400

    building out ingest logic for voter data

--- Makefile
+++ Makefile
@@ -8,10 +8,13 @@ pipeline-voter-data:
 	xfile --config config/extract_van_data.yaml --delimiter ',' --map voters static_data/ohio_rvht.csv \
 	> temp_data/voter_data.json
 
+	cat temp_data/voter_data.json | ngst --config config/ingest_van_data.yaml --target db --params=record_type:voter \
+	> temp_data/voter_id_map.jsonl
+
 
 pipeline-voting-history:
-	xfile --config config/extract_van_data.yaml --delimiter ',' --map voting_data static_data/ohio_rvht.csv \
+	xfile --config config/extract_van_data.yaml --delimiter ',' --map voting_history static_data/ohio_rvht.csv \
 	> temp_data/voting_history_data.json
 
-
-
+	cat temp_data/voting_history_data.json | ngst --config config/ingest_van_data.yaml --target db \
+	--params=record_type:voting_history > temp_data/voter_id_map.jsonl
---
+++ cvx_datastores.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+import json
+import uuid
+from snap import common
+from mercury.dataload import DataStore
+from mercury.mlog import mlog, mlog_err
+
+'''
+date_month = date_tokens[0]
+        date_day = date_tokens[1]
+        date_year = date_tokens[2]
+
+        day_id = olap.dim_id_for_value('dim_date_day', int(date_day))
+        month_id = olap.dim_id_for_value('dim_date_month', int(date_month))
+        year_id = olap.dim_id_for_value('dim_date_year', int(date_year))
+
+'''
+
+class ObjectFactory(object):
+    
+    @classmethod
+    def create_db_object(cls, table_name, db_svc, **kwargs):
+        DbObject = getattr(db_svc.Base.classes, table_name)
+        return DbObject(**kwargs)
+
+
+def to_boolean(value: str):
+
+    if not value:
+        return False
+    
+    if value.lower() == 'yes':
+        return True
+    
+    if value.lower() == 'no':
+        return False
+
+
+class PostgresDatastore(DataStore):
+
+    def __init__(self, service_object_registry, *channels, **kwargs):
+        super().__init__(service_object_registry, *channels, **kwargs)
+
+
+    def write_voter_data(self, voter_record, db_service, **write_params):
+
+        new_id = None
+
+        with db_service.txn_scope() as session:
+
+            db_rec = ObjectFactory.create_db_object('voter', db_service, **voter_record)
+            session.add(db_rec)
+            session.flush()
+            new_id = str(db_rec.id)
+
+        return new_id
+    
+
+    def write(self, records, **write_params):
+
+        postgres_svc = self.service_object_registry.lookup('postgres')        
+        record_type = write_params.get('record_type')
+
+        for raw_rec in records:
+
+            rec = json.loads(raw_rec)
+
+            if record_type == 'voter':
+                try:
+                    id = self.write_voter_data(rec, postgres_svc)
+                    
+                except Exception as err:
+                    mlog_err(err, issue=f"Error ingesting voter record.", record=rec)
+
+            else:
+                mlog(Exception(f'Unrecognized record type {record_type}'))
+
+     
+
